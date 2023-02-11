b87715eac1f3f207940450489a88791acd9f0d84
Likhith kumar <likhithmattapalli@gmail.com>
2023-02-12 01:12:14 +0530

    first commit

--- Modules/Authmodule.js
+++ Modules/Authmodule.js
@@ -1,5 +1,5 @@
 const jwt=require('jsonwebtoken')
-const usermodel=require('../Models/Usermodel')
+const usermodel=require('../Model/Usermodel')
 
 module.exports.authenticate=async(req,res,next)=>{
     if(!req.headers['access-token']) return res.status(400).send({msg:"token not found"})
